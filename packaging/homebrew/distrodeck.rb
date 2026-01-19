7449937fbedc2e401d6968b0ff2228f674dfa218
Nik Reljin <nikola.reljin@gmail.com>
2026-01-18 21:47:25 -0500

    fix: correct CI build action

--- packaging/homebrew/distrodeck.rb
+++ packaging/homebrew/distrodeck.rb
@@ -1,8 +1,8 @@
 class Distrodeck < Formula
   desc "Export and restore packages before distro upgrades"
   homepage "https://github.com/nikolareljin/distrodeck"
-  url "https://github.com/nikolareljin/distrodeck/releases/download/v0.3.0/distrodeck-0.3.0.tar.gz"
-  version "0.3.0"
+  url "https://github.com/nikolareljin/distrodeck/releases/download/v0.4.0/distrodeck-0.4.0.tar.gz"
+  version "0.4.0"
   sha256 "d4e3bd5fe30b8ce1fb277d570aace14cf80d4aa0c4310a233c36138a70d966f8"
   license "MIT"
 
