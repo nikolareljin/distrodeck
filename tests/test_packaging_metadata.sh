5f85b510c18fa7f0d73b3c8e2664b5cbb15240fa
Nik Reljin <nikola.reljin@gmail.com>
2026-08-25 13:47:41 -0400

    fix: run packaging check without ripgrep

--- tests/test_packaging_metadata.sh
+++ tests/test_packaging_metadata.sh
@@ -10,18 +10,18 @@ fail() {
     exit 1
 }
 
-rg -q '^override_dh_auto_build override_dh_auto_install:$' debian/rules \
+grep -q '^override_dh_auto_build override_dh_auto_install:$' debian/rules \
     || fail "Debian rules must bypass implicit Makefile build/install targets"
-rg -q '^\t@:$' debian/rules \
+grep -q '^[[:blank:]]*@:$' debian/rules \
     || fail "Debian build/install overrides must be no-ops"
-rg -q '^Version:[[:space:]]+[0-9]+(\.[0-9]+)+$' packaging/distrodeck.spec \
+grep -Eq '^Version:[[:space:]]+[0-9]+(\.[0-9]+)+$' packaging/distrodeck.spec \
     || fail "RPM Version must be a concrete package version"
-rg -q '^Source0:[[:space:]]+%\{name\}-%\{version\}\.tar\.gz$' packaging/distrodeck.spec \
+grep -Eq '^Source0:[[:space:]]+%\{name\}-%\{version\}\.tar\.gz$' packaging/distrodeck.spec \
     || fail "RPM spec must declare the source archive used by %autosetup"
-if rg -q '^Version:.*%\{version\}' packaging/distrodeck.spec; then
+if grep -q '^Version:.*%{version}' packaging/distrodeck.spec; then
     fail "RPM Version must not recursively expand the version macro"
 fi
-rg -q '^/usr/share/man/man1/distrodeck\.1\*$' packaging/distrodeck.spec \
+grep -Eq '^/usr/share/man/man1/distrodeck\.1\*$' packaging/distrodeck.spec \
     || fail "RPM %files must allow debuginfo compression of the man page"
-rg -q '^install -m 0644 distrodeck\.py ' packaging/distrodeck.spec \
+grep -q '^install -m 0644 distrodeck.py ' packaging/distrodeck.spec \
     || fail "RPM build must install every file declared in %files"
